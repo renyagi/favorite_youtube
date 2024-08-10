11550f74b842cebebaf9e488b91b9f2968c90575
renyagi <r.yagi@4nonome.com>
2024-08-11 01:55:12 +0900

    buttonの作成

---
+++ my-nextjs-app/src/components/Button/Button.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button_frame } from './Button';
+
+const meta: Meta<typeof Button_frame> = {
+  title: 'Components/Button_frame',
+  component: Button_frame,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button_frame>;
+
+export const Default: Story = {
+  args: {},
+};
---
+++ my-nextjs-app/src/components/Button/Button.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Button } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+import theme from '../../theme.js';
+
+interface Props {
+  label: string;
+  width: string;
+  onClick: () => void;
+}
+
+export const Button_frame = ({
+  label = "label",
+  width = "200px",
+  onClick,
+}: Props) => {
+  return (
+    <Button
+      backgroundColor={theme.colors.black}
+      borderColor={theme.colors.black}
+      borderRadius="12px"
+      onClick={onClick}
+      transition="0.2s"
+      width={width}
+      _hover={{
+        bg: theme.colors.gray,
+        borderColor: theme.colors.gray,
+      }}
+    >
+      <Text>
+        {label}
+      </Text>
+    </Button>
+  );
+};
+
+const Text = styled.div`
+  background-color: inherit;
+  color: ${theme.colors.white};
+  font-size: ${theme.fontSizes.md};
+  font-weight: ${theme.fontWeights.bold};
+  line-height: 16px;
+  margin: 16px 24px;
+  text-align: center;
+  width: 100%;
+`;
+
