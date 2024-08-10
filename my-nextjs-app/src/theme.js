79c0850f3c8fff344fb8c904aa59773c150af405
renyagi <r.yagi@4nonome.com>
2024-08-11 01:54:56 +0900

    themeの作成

---
+++ my-nextjs-app/src/theme.js
@@ -0,0 +1,29 @@
+const theme = {
+  fonts: {
+    heading: 'Roboto, sans-serif',
+    body: 'Roboto, sans-serif',
+  },
+  fontSizes: {
+    sm: '14px',
+    md: '16px',
+    lg: '18px',
+    xl: '20px',
+    '2xl': '24px',
+    '3xl': '30px',
+    '4xl': '36px',
+    '5xl': '48px',
+  },
+  fontWeights: {
+    normal: 400,
+    medium: 500,
+    bold: 700,
+    extraBold: 900,
+  },
+  colors: {
+    black: "#141416",
+    gray: "#D9D9D9",
+    white: "#FCFCFD",
+  },
+}
+
+export default theme;
