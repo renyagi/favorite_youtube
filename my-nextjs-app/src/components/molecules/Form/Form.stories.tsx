21ab9b9cc0acbfc56664b5b0038155653102c933
renyagi <r.yagi@4nonome.com>
2024-08-11 14:34:08 +0900

    Formの作成

---
+++ my-nextjs-app/src/components/molecules/Form/Form.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Form_frame } from "./Form";
+
+const meta: Meta<typeof Form_frame> = {
+  title: "Components/Form_frame",
+  component: Form_frame,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Form_frame>;
+
+export const Default: Story = {
+  args: {},
+};
