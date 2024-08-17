a564483f01b950e5e6ebe873765f2a1b44fdf0ed
renyagi <r.yagi@4nonome.com>
2024-08-18 00:18:25 +0900

    実行環境構築

---
+++ my-nextjs-app/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      Login
+    </main>
+  );
+}
