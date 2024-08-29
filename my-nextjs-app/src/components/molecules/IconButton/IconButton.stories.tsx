05f6e6081a43309e019cbab191bb596126dead9c
renyagi <r.yagi@4nonome.com>
2024-08-29 23:59:36 +0900

    IconButtonの作成

---
+++ my-nextjs-app/src/components/molecules/IconButton/IconButton.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { IconButton } from "./IconButton";
+
+const meta: Meta<typeof IconButton> = {
+  title: "Components/Molecules/IconButton",
+  component: IconButton,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof IconButton>;
+
+export const Default: Story = {
+  args: {},
+};
