f2baf56b8fe69115a224215f4f05338c3c8dfdb6
renyagi <r.yagi@4nonome.com>
2024-09-13 23:50:34 +0900

    Userデータの作成

--- my-backend/server/urls.py
+++ my-backend/server/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from api import views as api_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- my-nextjs-app/src/app/login/page.tsx
+++ my-nextjs-app/src/app/login/page.tsx
@@ -1,15 +1,31 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { LoginPage } from "../../sections/login/LoginPage/LoginPage";
+import axios from "axios";
 
 export default function Page() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleClick = async (email, password) => {
+    try {
+      await axios.post("http://localhost:8000/api/user/", {
+        email: email,
+        password: password,
+      });
+    } catch (error) {
+      // エラーの処理
+      console.error('Error occurred:', error);
+    }
+  };
+
   return (
     <LoginPage
-    // email={email}
-    // href={href}
-    // password={password}
-    // onClick={onClick}
+      email={email}
+      href="#"
+      password={password}
+      onClick={handleClick}
     />
   );
 }
